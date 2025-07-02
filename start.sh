c3c7d160b95bfde2e9d1cc6f4d88122ff240dbf3
jayaraj-kp <jayarajkprajkp@gmail.com>
2025-07-02 12:37:56 +0530

    updates odoo.conf and start.sh

--- start.sh
+++ start.sh
@@ -1,2 +1,8 @@
 
-exec python3 odoo-bin -c odoo.conf
+#!/bin/bash
+
+# Print the port so you see it in logs
+echo "Render PORT = $PORT"
+
+# Start Odoo on the correct dynamic port
+python3 odoo-bin -c odoo.conf --http-port=$PORT
